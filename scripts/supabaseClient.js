3186c7e9c60e371bc6f2fc2f96c99157eaae8b37
sarar-git <workatsararops@gmail.com>
2025-06-27 15:22:14 +0530

    Create supabaseClient.js

---
+++ scripts/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://wllmlchqjnswuduftihd.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsbG1sY2hxam5zd3VkdWZ0aWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNjQ0MTIsImV4cCI6MjA2NTg0MDQxMn0.hNNiXHMvmfy3eW_d2VeWQOJVFuUOH0qoqWE007-7vhM'
+);
