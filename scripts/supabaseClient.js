1aa61fa37e00d6928f1bc6acd6361c1091fcc1de
sarar-git <workatsararops@gmail.com>
2025-06-28 19:00:21 +0530

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabase = 'https://wllmlchqjnswuduftihd.supabase.co',
-const supabase = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsbG1sY2hxam5zd3VkdWZ0aWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNjQ0MTIsImV4cCI6MjA2NTg0MDQxMn0.hNNiXHMvmfy3eW_d2VeWQOJVFuUOH0qoqWE007-7vhM'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  "https://wllmlchqjnswuduftihd.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsbG1sY2hxam5zd3VkdWZ0aWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNjQ0MTIsImV4cCI6MjA2NTg0MDQxMn0.hNNiXHMvmfy3eW_d2VeWQOJVFuUOH0qoqWE007-7vhM"
+);
