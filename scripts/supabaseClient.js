4326b8e56a793d842e28ccd480c37676fd51c973
sarar-git <workatsararops@gmail.com>
2025-06-29 09:41:03 +0530

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.5";
 
 export const supabase = createClient(
   "https://wllmlchqjnswuduftihd.supabase.co",
